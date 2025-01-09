390cb2df1c5461fe92c7d062b2c1ec4c6e633c1e
fredericoaugusto <fred3112.augusto@gmail.com>
2025-01-09 17:43:05 -0300

    Criação do projeto, controller, rotas e página home.

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class MainController extends Controller
+{
+    public function home(): View
+    {
+        return view('home');
+    }
+
+    public function generateExercises(Request $request)
+    {
+        $request->validate([
+            'check_sum' => 'required_without_all:check_subtraction,check_multiplication,check_division',
+            'check_subtraction' => 'required_without_all:check_sum,check_multiplication,check_division',
+            'check_multiplication' => 'required_without_all:check_sum,check_subtraction,check_division',
+            'check_division' => 'required_without_all:check_sum,check_subtraction,check_multiplication',
+
+            'number_one' => 'required|integer|min:0|max:999',
+            'number_two' => 'required|integer|min:0|max:999',
+
+            'number_exercises' => 'required|integer|min:5|max:50',
+            ]);
+
+
+        dd($request->all());
+
+    }
+
+    public function printExercises()
+    {
+        echo 'printExercises';
+    }
+
+    public function exportExercises()
+    {
+        echo 'exportExercises';
+    }
+}
