4b2472fa5adea87def48aa0f0e31590402167bcf
fredericoaugusto <fred3112.augusto@gmail.com>
2025-01-10 17:55:44 -0300

    Lógica inicial controller

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -20,14 +20,58 @@ class MainController extends Controller
             'check_multiplication' => 'required_without_all:check_sum,check_subtraction,check_division',
             'check_division' => 'required_without_all:check_sum,check_subtraction,check_multiplication',
 
-            'number_one' => 'required|integer|min:0|max:999',
+            'number_one' => 'required|integer|min:0|max:999|lt:number_two',
             'number_two' => 'required|integer|min:0|max:999',
 
             'number_exercises' => 'required|integer|min:5|max:50',
             ]);
 
+            $operations = [];
+            $operations[] = $request->check_sum ? 'sum' : '';
+            $operations[] = $request->check_subtraction ? 'subtraction' : '';
+            $operations[] = $request->check_multiplication ? 'multiplication' : '';
+            $operations[] = $request->check_division ? 'division' : '';
 
-        dd($request->all());
+            $min = $request->number_one;
+            $max = $request->number_two;
+
+            $numberExercises = $request->number_exercises;
+
+            $exercises = [];
+            for($index = 0; $index <= $numberExercises; $index++) {
+                $operation = $operations[array_rand($operations)];
+                $number1 = rand($min, $max);
+                $number2 = rand($min, $max);
+
+                $exercise = '';
+                $solution = '';
+
+                switch($operation) {
+                    case 'sum':
+                        $exercise = "$number1 + $number2 = ";
+                        $solution = $number1 + $number2;
+                        break;
+                    case 'subtraction':
+                        $exercise = "$number1 - $number2 = ";
+                        $solution = $number1 - $number2;
+                        break;
+                    case 'multiplication':
+                        $exercise = "$number1 * $number2 = ";
+                        $solution = $number1 * $number2;
+                        break;
+                    case 'division':
+                        $exercise = "$number1 / $number2 = ";
+                        $solution = $number1 / $number2;
+                        break;
+                }
+
+                $exercises[] = [
+                    'exercise_number' => $index,
+                    'exercise' => $exercise,
+                    'solution' => "$exercise $solution",
+                ];
+            }
+            dd($exercises);
 
     }
 
